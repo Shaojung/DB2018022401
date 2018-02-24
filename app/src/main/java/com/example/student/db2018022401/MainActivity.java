fa423b047ec295a4939bbc4de005957381718f9e
Shaojung <shaojung@gmail.com>
2018-02-24 10:21:20 +0800

    custom layout in simpleadapter

--- app/src/main/java/com/example/student/db2018022401/MainActivity.java
+++ app/src/main/java/com/example/student/db2018022401/MainActivity.java
@@ -16,30 +16,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lv = (ListView) findViewById(R.id.listView);
-        ArrayList<Map<String, String>> mylist = new ArrayList();
-        HashMap<String, String> m1 = new HashMap();
+        ArrayList<Map<String, Object>> mylist = new ArrayList();
+        HashMap<String, Object> m1 = new HashMap();
         m1.put("city", "台北");
         m1.put("code", "02");
+        m1.put("img", R.drawable.tpe);
         mylist.add(m1);
 
-        HashMap<String, String> m2 = new HashMap();
+        HashMap<String, Object> m2 = new HashMap();
         m2.put("city", "台中");
         m2.put("code", "04");
+        m2.put("img", R.drawable.tc);
         mylist.add(m2);
 
-        HashMap<String, String> m3 = new HashMap();
+        HashMap<String, Object> m3 = new HashMap();
         m3.put("city", "台南");
         m3.put("code", "06");
+        m3.put("img", R.drawable.tn);
         mylist.add(m3);
 
-        HashMap<String, String> m4 = new HashMap();
+        HashMap<String, Object> m4 = new HashMap();
         m4.put("city", "高雄");
         m4.put("code", "07");
+        m4.put("img", R.drawable.kh);
         mylist.add(m4);
 
         SimpleAdapter adapter = new SimpleAdapter(MainActivity.this,
-                mylist, R.layout.myitem, new String[] {"city", "code"},
-                new int[] {R.id.textView, R.id.textView2});
+                mylist, R.layout.myitem, new String[] {"city", "code", "img"},
+                new int[] {R.id.textView, R.id.textView2, R.id.imageView});
         lv.setAdapter(adapter);
 
     }
